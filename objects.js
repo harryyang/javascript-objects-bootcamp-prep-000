f0579505c23d89c45e4e9b443a88a3f851cd06e2
Harold Yang <harold.h.yang@gmail.com>
2017-09-15 18:16:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {adelle: "hello"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign ({}, playlist, {[artistName]: songTitle} )
+  return Object.assign ({}, playlist, {[artistName]: songTitle});
 }
 
 
